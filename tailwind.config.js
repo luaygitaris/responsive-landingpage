8088dbe34bec23cca55e47461acc6cd49e7ae6d3
luaycom <luaygitaris@gmail.com>
2024-08-20 13:48:36 +0700

    add section your data

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       backgroundColor: {
         blue1: "#4F9CF9",
       },
+      backgroundImage: {
+        bgsatu: "url(img/background/Bg-1.png",
+      },
     },
   },
   plugins: [],
